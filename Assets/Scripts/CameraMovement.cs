3293745b4d684c4a53bc2178b1276c059f9b2ca8
Zainab Ali <zainab_ali96@outlook.com>
2021-03-10 15:41:12 +0400

    Assignment 2 Commit
    Add Camera Movement to view Entire Terrain

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float cameraSpeed;
+    public GameObject target;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, cameraSpeed * Time.deltaTime, 0);
+
+        //transform.RotateAround(target.transform.position, Vector3.up, cameraSpeed * Time.deltaTime);
+    }
+}
